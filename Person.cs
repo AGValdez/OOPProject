3785a92fbf5e85b18bcbad4b3913149d73298683
Alec Greef Valdez <alecgreefzedlav@gmail.com>
2018-11-23 09:43:57 +0800

    Added Files
    Added Calculations.cs, Person.cs, Product.cs

---
+++ Person.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP_project
+{
+    class Person
+    {
+        public string FirstName;
+        public string MiddleInitial;
+        string LastName;
+        public string BirthDate;
+        public string Address;
+
+        public Person()
+        {
+
+        }
+
+        static string GetFullName(string firstName, string middleInitial, string lastName)
+        {
+            return null;
+        }
+        static int GetAge()
+        {
+            return 0;
+        }
+    }
+}
